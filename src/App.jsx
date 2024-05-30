f99520b05251f9dc46277a00d3b12e460938cbe5
chakradharkotagiri <chakradharkotagiri111@gmail.com>
2024-05-30 19:54:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-xl font-bold text-cyan-700'>Hello Tailwindcss with react vite</h1>
+    </>
+  )
+}
+
+export default App
