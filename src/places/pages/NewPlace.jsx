b56a3ab2ec618caa343e57ce49a913e33e4bb66d
chakradharkotagiri <chakradharkotagiri111@gmail.com>
2025-12-18 15:42:52 +0530

    Cloudinary Image url issue fixed

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -44,7 +44,7 @@ const NewPlace = () => {
   const uploadImageToCloudinary = async (file) => {
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", "unsigned_users");
+    formData.append("upload_preset", "frontend_unsigned");
 
     const res = await fetch(
       "https://api.cloudinary.com/v1_1/dmdrtetyn/image/upload",
